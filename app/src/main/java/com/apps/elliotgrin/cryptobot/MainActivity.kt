af67c04a6383b841d5117c8e883baddab367a1bf
rkuznetsov1 <rpkuznetsov@gmail.com>
2017-12-28 00:39:45 +0300

    Add data binding

--- app/src/main/java/com/apps/elliotgrin/cryptobot/MainActivity.kt
+++ app/src/main/java/com/apps/elliotgrin/cryptobot/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.apps.elliotgrin.cryptobot
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.apps.elliotgrin.cryptobot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        
     }
 }
